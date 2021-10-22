5e0c636acc36532af12f0f2dde1b3cbd4b12d433
kateabr <kateabr@yandex.ru>
2021-10-22 17:37:25 +0300

    refresh token controller

---
+++ src/main/java/kika/controller/JwtController.java
@@ -0,0 +1,30 @@
+package kika.controller;
+
+import javax.servlet.http.HttpServletResponse;
+import kika.configuration.security.SecurityConfiguration;
+import kika.security.jwt.encode.JwtCookieService;
+import kika.security.jwt.encode.JwtToken;
+import kika.security.jwt.encode.JwtTokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/login")
+class JwtController {
+    private final JwtTokenService jwtTokenService;
+    private final JwtCookieService jwtCookieService;
+
+    @PostMapping("/refresh")
+    public void refresh(
+        @RequestHeader(SecurityConfiguration.REFRESH_TOKEN_COOKIE_NAME) String refreshToken,
+        HttpServletResponse response
+    ) {
+        JwtToken token = jwtTokenService.refresh(refreshToken);
+        response.addCookie(jwtCookieService.accessCookie(token.accessToken()));
+        response.addCookie(jwtCookieService.refreshCookie(token.refreshToken()));
+    }
+}
