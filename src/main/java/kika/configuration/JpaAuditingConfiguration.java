9ffb9baa977fd66cde4c893cd7779d70b4fa20dd
kateabr <kateabr@yandex.ru>
2021-10-22 17:36:57 +0300

    add new principal to JPA auditing

--- src/main/java/kika/configuration/JpaAuditingConfiguration.java
+++ src/main/java/kika/configuration/JpaAuditingConfiguration.java
@@ -1,6 +1,7 @@
 package kika.configuration;
 
 import java.util.Optional;
+import kika.security.principal.KikaPrincipal;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -16,12 +17,11 @@ class JpaAuditingConfiguration {
         return () -> {
             Authentication auth = SecurityContextHolder.getContext().getAuthentication();
             if (auth == null) {
-                return Optional.of("SYSTEM");
+                return Optional.of("-1");
             } else {
                 Object principal = auth.getPrincipal();
-                if (principal instanceof String) {
-//                if (principal instanceof Authentication) {
-                    return Optional.of(principal.toString());
+                if (principal instanceof KikaPrincipal kikaPrincipal) {
+                    return Optional.of(String.valueOf(kikaPrincipal.accountId()));
                 } else {
                     throw new IllegalStateException(String.format("Unknown principal %s", principal.toString()));
                 }
