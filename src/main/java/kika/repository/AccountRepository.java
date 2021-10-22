6e110689b9e60f6394c581c11934a8d55c36e9af
kateabr <kateabr@yandex.ru>
2021-10-22 17:28:57 +0300

    add a method to query account by refresh token

--- src/main/java/kika/repository/AccountRepository.java
+++ src/main/java/kika/repository/AccountRepository.java
@@ -2,10 +2,22 @@ package kika.repository;
 
 import kika.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.Optional;
 
 @Repository
 @Transactional
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    @Query("select A from Account A where A.provider=:provider and A.providerId=:providerId")
+    Optional<Account> findByProviderId(
+        @Param("provider") Account.Provider provider,
+        @Param("providerId") String providerId
+    );
+
+    @Query("select A from Account A where A.refreshToken=:token")
+    Optional<Account> findByRefreshToken(@Param("token") String token);
 }
